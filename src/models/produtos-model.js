62df306478bfb63936109f281826ee199af67a5a
Paulo Gonçalves <paulorochag@hotmail.com>
2020-05-01 21:27:02 -0300

    feat(produtos): implementado o GET da rota 'produtos'

---
+++ src/models/produtos-model.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { Joi } = require('express-validation')
+
+exports.schemaGet = {
+  query: Joi.object({
+    _id: Joi.any(),
+    nome: Joi.any(),
+    preco: Joi.any(),
+    descricao: Joi.any(),
+    quantidade: Joi.any()
+  })
+}
+
+exports.schemaPost = {
+  body: Joi.object({
+    nome: Joi.string().required(),
+    preco: Joi.number().positive().integer().required(),
+    descricao: Joi.string().required(),
+    quantidade: Joi.number().min(0).integer().required()
+  })
+}
+
+exports.schemaDelete = {
+  params: Joi.object({
+    id: Joi.string().required()
+  })
+}
+
+exports.schemaPut = {
+  params: Joi.object({
+    id: Joi.string().required()
+  }),
+  body: Joi.object({
+    nome: Joi.string().required(),
+    preco: Joi.number().positive().integer().required(),
+    descricao: Joi.string().required(),
+    quantidade: Joi.number().min(0).integer().required()
+  })
+}
