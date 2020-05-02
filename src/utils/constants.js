00352a8b97011e8d56581965e88fcf768d68c020
Paulo Gonçalves <paulorochag@hotmail.com>
2020-05-02 16:18:37 -0300

    refactor: centralização de mensagem para melhor manutenção

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,5 +7,6 @@ module.exports = {
   DELETE_NONE: 'Nenhum registro excluído',
   PUT_SUCESS: 'Registro alterado com sucesso',
   LOGIN_SUCESS: 'Login realizado com sucesso',
-  LOGIN_FAIL: 'Email e/ou senha inválidos'
+  LOGIN_FAIL: 'Email e/ou senha inválidos',
+  EMAIL_JA_USADO: 'Este email já está sendo usado'
 }
