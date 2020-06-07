5d38dbe5fa43964a8bc2e522d53fa66a791a3001
Paulo Gonçalves <paulorochag@hotmail.com>
2020-06-07 02:00:36 -0300

    ci: implementar entrega contínua
    todo push na master será totalmente validado e, dependendo da alteração, gerará automaticamente uma
    nova relase.
    
    fix #25

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
-# Changelog
-
-All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
 ## [2.2.0](https://github.com/PauloGoncalvesBH/serverest/compare/v2.1.5...v2.2.0) (2020-06-06)
 
 
---
+++ release.config.js
@@ -0,0 +1,29 @@
+/* eslint no-template-curly-in-string: 0 */
+
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { type: 'docs', scope: 'readme', release: 'patch' },
+        { type: 'docs', scope: 'api-doc', release: 'patch' },
+        { scope: 'no-release', release: false }
+      ],
+      parserOpts: {
+        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+      }
+    },
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogTitle: 'Changelog'
+    }],
+    ['@semantic-release/npm', {
+      tarballDir: 'dist'
+    }],
+    ['@semantic-release/git', {
+      message: 'chore(release): ${nextRelease.version}'
+    }],
+    ['@semantic-release/github', {
+      assets: 'dist/*.tgz'
+    }]
+  ]
+}
