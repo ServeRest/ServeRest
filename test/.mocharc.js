45480950a4195350997dc50973581ee37ef5df77
Paulo Gonçalves <paulorochag@hotmail.com>
2020-08-09 17:26:01 -0300

    test(mutation): implementar testes de mutação
    fix #86

--- test/.mocharc.js
+++ test/.mocharc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  reporter: 'list',
   require: 'test/utils/global.js',
-  retries: 1,
-  spec: ["test/**/*.test.js"],
-  timeout: 2000
+  spec: ['test/**/*.test.js'],
+  timeout: 30000
 }
