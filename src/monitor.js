1e647b397f602ff10cbfea8c9ec937799b9957f5
Paulo Gonçalves <paulorochag@hotmail.com>
2020-09-05 13:01:05 -0300

    refactor(monitor): melhorar legibilidade de quando o monitoramento é habilitado

--- src/monitor.js
+++ src/monitor.js
@@ -10,16 +10,16 @@ Esse arquivo está marcado para ser ignorado no arquivo stryker.conf.js
 const moesif = require('moesif-nodejs')
 const os = require('os')
 
+const ehAmbienteDeDesenvolvimento = process.env.NODE_ENV === 'serverest-development'
+const ehAmbienteDeTestes = process.env.NODE_ENV === 'serverest-test'
 const moesifMiddleware = moesif({
   applicationId: 'eyJhcHAiOiIxNTA6MTU1MCIsInZlciI6IjIuMCIsIm9yZyI6IjQ5MToxMTIxIiwiaWF0IjoxNTk4OTE4NDAwfQ.e0E6Qhz1o1Jjs5prulHDYEBlv0juruWs_btjq2mong8',
   identifyUser: (req, res) => { return os.userInfo().username }
 })
 
 module.exports = app => {
-  const ehAmbienteDeDesenvolvimento = process.env.NODE_ENV === 'serverest-developmenat'
-  const ehAmbienteDeTestes = process.env.NODE_ENV === 'serverest-test'
-
-  if (!ehAmbienteDeDesenvolvimento && !ehAmbienteDeTestes) {
-    app.use(moesifMiddleware)
+  if (ehAmbienteDeDesenvolvimento || ehAmbienteDeTestes) {
+    return
   }
+  app.use(moesifMiddleware)
 }
