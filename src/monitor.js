8b1abb75550138d0107e9c99278bed14ffc7ec7d
Paulo Gonçalves <paulorochag@hotmail.com>
2020-09-11 18:11:09 -0300

    feat(monitoramento): identificar versão utilizada

--- src/monitor.js
+++ src/monitor.js
@@ -10,11 +10,14 @@ Esse arquivo está marcado para ser ignorado no arquivo stryker.conf.js
 const moesif = require('moesif-nodejs')
 const os = require('os')
 
+const { version: serverestVersion } = require('../package.json')
+
 const ehAmbienteDeDesenvolvimento = process.env.NODE_ENV === 'serverest-development'
 const ehAmbienteDeTestes = process.env.NODE_ENV === 'serverest-test'
 const moesifMiddleware = moesif({
   applicationId: 'eyJhcHAiOiIxNTA6MTU1MCIsInZlciI6IjIuMCIsIm9yZyI6IjQ5MToxMTIxIiwiaWF0IjoxNTk4OTE4NDAwfQ.e0E6Qhz1o1Jjs5prulHDYEBlv0juruWs_btjq2mong8',
-  identifyUser: (req, res) => { return os.userInfo().username }
+  identifyUser: (req, res) => { return os.userInfo().username },
+  identifyCompany: (req, res) => { return serverestVersion }
 })
 
 module.exports = app => {
