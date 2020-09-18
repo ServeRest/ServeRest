974303e13403ca57deb572c6ffc5a01ca9690aff
Paulo Gonçalves <paulorochag@hotmail.com>
2020-09-17 23:43:56 -0300

    fix: traduzir mensagem 'must be of type object'  (#118)
    * style: ordenar alfabeticamente os tipos de erro
    
    * fix(montarmensagemdeerrodeschema): traduzir 'object.base'
    
    fix #114
    
    * chore: atualizar mocha

--- src/utils/montarMensagemDeErroDeSchema.js
+++ src/utils/montarMensagemDeErroDeSchema.js
@@ -6,18 +6,19 @@ module.exports = error => {
   detalhesDoErro.forEach(erro => {
     const propriedade = erro.context.label
     const mapeamentoDoErro = {
+      'any.only': `${propriedade} deve ser 'true' ou 'false'`,
       'any.required': `${propriedade} é obrigatório`,
-      'string.email': `${propriedade} deve ser um email válido`,
-      'string.empty': `${propriedade} não pode ficar em branco`,
-      'string.base': `${propriedade} deve ser uma string`,
-      'object.unknown': `${propriedade} não é permitido`,
-      'array.includesRequiredUnknowns': `${propriedade} não contém 1 valor obrigatório`,
       'array.base': `${propriedade} deve ser um array`,
+      'array.includesRequiredUnknowns': `${propriedade} não contém 1 valor obrigatório`,
       'number.base': `${propriedade} deve ser um número`,
       'number.integer': `${propriedade} deve ser um inteiro`,
-      'number.positive': `${propriedade} deve ser um número positivo`,
       'number.min': `${propriedade} deve ser maior ou igual a 0`,
-      'any.only': `${propriedade} deve ser 'true' ou 'false'`,
+      'number.positive': `${propriedade} deve ser um número positivo`,
+      'object.base': `${propriedade} deve ser um objeto`,
+      'object.unknown': `${propriedade} não é permitido`,
+      'string.base': `${propriedade} deve ser uma string`,
+      'string.email': `${propriedade} deve ser um email válido`,
+      'string.empty': `${propriedade} não pode ficar em branco`,
       default: `${erro.message} - Erro ${erro.type} - Abra uma issue informando essa resposta. https://github.com/PauloGoncalvesBH/ServeRest/issues`
     }
     /* istanbul ignore next */
