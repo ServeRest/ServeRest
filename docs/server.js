3702467ffdf471e3d21c51a99b5cf3daf6d125a6
Paulo Gonçalves <paulorochag@hotmail.com>
2020-10-16 15:40:09 -0300

    chore: monitorar acessos na documentação em serverest.dev

--- docs/server.js
+++ docs/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const moesif = require('moesif-nodejs')
 const { join } = require('path')
 
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/favicon.ico', (req, res) => { res.sendStatus(204) })
+
+app.use(moesif({
+  applicationId: 'eyJhcHAiOiIxNTA6MTU1MCIsInZlciI6IjIuMCIsIm9yZyI6IjQ5MToxMTIxIiwiaWF0IjoxNTk4OTE4NDAwfQ.e0E6Qhz1o1Jjs5prulHDYEBlv0juruWs_btjq2mong8',
+  identifyUser: (req, res) => { return 'serverest.dev' }
+}))
+
+app.get('*', function (req, res) {
   res.sendFile(join(__dirname, 'index.html'))
 })
 
