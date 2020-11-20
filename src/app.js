052e3615e8482c67fac835f18ddb474560c61a70
Paulo Gonçalves <paulorochag@hotmail.com>
2020-11-20 11:45:50 -0300

    docs(serverest): implementar nova documentação com swagger

--- src/app.js
+++ src/app.js
@@ -8,12 +8,14 @@ const morgan = require('morgan')
 const queryParser = require('express-query-int')
 const timeout = require('connect-timeout')
 const { join } = require('path')
+const swaggerUi = require('swagger-ui-express')
 
 const { formaDeExecucao, urlDocumentacao } = require('./utils/ambiente')
 const { conf } = require('./utils/conf')
 const errorHandler = require('./middlewares/error-handler')
 const logger = require('./utils/logger')
 const { version } = require('../package.json')
+const swaggerDocument = require('../docs/swagger.json')
 
 const ehAmbienteDeTestes = process.env.NODE_ENV === 'serverest-test'
 
@@ -50,21 +52,26 @@ if (formaDeExecucao() === 'serverest.dev' || formaDeExecucao() === 'agilizei') {
 logger(app)
 
 /* istanbul ignore next */
-app.get('/', async (req, res) => {
-  let pathDocumentacao
-  switch (formaDeExecucao()) {
-    case 'serverest.dev':
-      pathDocumentacao = '../docs/serverest.dev.html'
-      break
-    case 'agilizei':
-      pathDocumentacao = '../docs/agilizei.html'
-      break
-    default:
-      pathDocumentacao = '../docs/localhost.html'
-  }
-  res.sendFile(join(__dirname, pathDocumentacao))
-})
-app.get('/favicon.ico', (req, res) => { res.sendFile(join(__dirname, '../docs/favicon.png')) })
+switch (formaDeExecucao()) {
+  case 'serverest.dev':
+    swaggerDocument.host = 'serverest.dev'
+    break
+  case 'agilizei':
+    swaggerDocument.host = 'serverest-api.agilizei.com'
+    break
+}
+
+const uiOptions = {
+  customSiteTitle: 'ServeRest',
+  customfavIcon: '/favicon.ico',
+  customCss: `
+  .topbar-wrapper img { content:url(https://user-images.githubusercontent.com/29241659/118382797-365f3900-b5cf-11eb-9c82-0298a5c75b7e.png); width:150px; height:auto; }
+  .swagger-ui .topbar { background-color: #000000; border-bottom: 20px solid #7900e2; }`
+}
+
+app.use('/', swaggerUi.serve)
+app.get('/', swaggerUi.setup(swaggerDocument, uiOptions))
+app.use('/favicon.ico', express.static(join(__dirname, '../docs/favicon.png')))
 app.get('/version', (req, res) => { res.status(200).send({ version }) })
 
 /* istanbul ignore if */
