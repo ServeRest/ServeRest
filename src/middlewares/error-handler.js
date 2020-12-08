b585918048837325eb5053ecede39901b6e3d2b8
Paulo Gonçalves <paulorochag@hotmail.com>
2020-12-07 21:51:42 -0300

    test: validar comportamento em cenário de exceção (500)
    com a implementação de stub foi possível a validação de cenário de exceção e a retirada de exceção
    de code coverage.
    
    closes #182

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -3,15 +3,10 @@ const montarMensagemDeErroDeSchema = require('../utils/montarMensagemDeErroDeSch
 
 function errorHandler (error, _req, res, _next) {
   const erroDeSchema = error.name === 'ValidationError'
-  /* istanbul ignore else */
   if (erroDeSchema) {
     return res.status(400).json(montarMensagemDeErroDeSchema(error))
-  } else if (error instanceof SyntaxError && error.status === 400) {
-    return res.sendStatus(400)
-  } else {
-    console.error(error)
-    return res.status(500).json({ message: INTERNAL_ERROR, error })
   }
+  return res.status(500).json({ message: INTERNAL_ERROR, error })
 }
 
 module.exports = errorHandler
---
+++ test/outros/error-handler.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai')
+const sandbox = require('sinon').createSandbox()
+
+const carrinhosService = require('../../src/services/carrinhos-service.js')
+
+describe('Error handler', () => {
+  afterEach(() => sandbox.restore())
+
+  it('Deve informar para abrir issue ao ocorrer erro 500', async () => {
+    sandbox.stub(carrinhosService, 'getAll').throws('Teste de erro 500')
+
+    const { body } = await request.get('/carrinhos').expect(500)
+
+    chai.assert.deepEqual(body, {
+      message: 'Abra uma issue informando essa resposta. https://github.com/PauloGoncalvesBH/ServeRest/issues',
+      error: { name: 'Teste de erro 500' }
+    })
+  })
+})
