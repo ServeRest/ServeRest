06ae564cb2951f067562b7481379bd26611f2e6a
Paulo Gonçalves <paulorochag@hotmail.com>
2021-03-06 21:54:45 -0300

    fix: informar o usuário sobre como lidar com o erro 'entity.parse.failed' enquanto investigo solução
    Está sendo investigado na issue #225

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -6,6 +6,13 @@ function errorHandler (error, _req, res, _next) {
   if (erroDeSchema) {
     return res.status(400).json(montarMensagemDeErroDeSchema(error))
   }
+  /* istanbul ignore if */
+  if (error.type === 'entity.parse.failed') {
+    console.log('Erro 500')
+    return res.status(500).json({
+      message: 'Adicione aspas em todos os valores. Esse problema já está sendo investigado na issue https://github.com/PauloGoncalvesBH/ServeRest/issues/225'
+    })
+  }
   return res.status(500).json({ message: INTERNAL_ERROR, error })
 }
 
