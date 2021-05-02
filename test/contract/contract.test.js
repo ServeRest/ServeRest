6c5f863d6381c419fbdea6701564e7802c2566f7
Paulo Gonçalves <paulorochag@hotmail.com>
2021-05-02 16:26:44 -0300

    test(contrato): validar integração entre front e back

---
+++ test/contract/contract.test.js
@@ -0,0 +1,50 @@
+const http = require('http')
+const isCI = require('is-ci')
+const { Verifier } = require('@pact-foundation/pact')
+
+const app = require('../../src/app')
+
+describe('ServeRest - Verificação do contrato', () => {
+  const SERVER_URL = 'http://localhost:3001'
+  const server = http.createServer(app)
+
+  const gitHash = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString()
+    .trim()
+
+  const gitBranch = require('child_process')
+    .execSync('git branch --show-current')
+    .toString()
+    .trim()
+
+  before(() => {
+    server.listen(3001, () => {
+      console.log(`Clients Service listening on ${SERVER_URL}`)
+    })
+  })
+
+  after(() => {
+    server.close()
+  })
+
+  it('Validates the expectations of ServeRest', () => {
+    const options = {
+      provider: 'ServeRest - API Rest',
+      logLevel: 'INFO',
+      pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
+      pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+      providerBaseUrl: SERVER_URL,
+      providerVersionTags: gitBranch,
+      providerVersion: gitHash,
+      publishVerificationResult: isCI
+    }
+
+    return new Verifier(options)
+      .verifyProvider()
+      .then((output) => {
+        console.log('Pact Verification Complete!')
+        console.log(output)
+      })
+  })
+})
--- test/integration/.mocharc.js
+++ test/integration/.mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
   require: 'test/integration/utils/global.js',
-  spec: ['test/**/*.test.js'],
+  spec: ['test/integration/**/*.test.js'],
   timeout: 30000
 }
