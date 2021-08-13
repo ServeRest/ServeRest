72b0767e2e9666ff15769c87403f89ca9535cfcd
Paulo Gonçalves <paulorochag@hotmail.com>
2021-08-13 00:42:55 -0300

    refactor(database): alterar o banco de dados para um fork atualizado e com promise implementado

--- src/services/usuarios-service.js
+++ src/services/usuarios-service.js
@@ -1,89 +1,42 @@
 'use strict'
 
-const Nedb = require('nedb')
+const Datastore = require('nedb-promises')
 const { join } = require('path')
 
 const alterarValoresParaRegex = require('../utils/alterarValoresParaRegex')
 
-const datastore = new Nedb({ filename: join(__dirname, '../data/usuarios.db'), autoload: true })
+const datastore = Datastore.create({ filename: join(__dirname, '../data/usuarios.db'), autoload: true })
 
 exports.getAll = queryString => {
   queryString = alterarValoresParaRegex(queryString)
-  return new Promise((resolve, reject) => {
-    datastore.find(queryString, (err, resultado) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(resultado)
-    })
-  })
+  return datastore.find(queryString)
 }
 
 exports.getOne = id => {
-  return new Promise((resolve, reject) => {
-    datastore.findOne({ _id: id }, (err, usuario) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(usuario)
-    })
-  })
+  return datastore.findOne({ _id: id })
 }
 
 exports.getDadosDoUsuario = queryString => {
-  return new Promise((resolve, reject) => {
-    datastore.findOne(queryString, (err, resultado) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(resultado)
-    })
-  })
+  return datastore.findOne(queryString)
 }
 
-exports.existeUsuario = pesquisa => {
-  return new Promise((resolve, reject) => {
-    datastore.count(pesquisa, (err, count) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(count !== 0)
-    })
-  })
+exports.existeUsuario = async pesquisa => {
+  return datastore.count(pesquisa)
 }
 
-exports.usuarioEhAdministrador = ({ email, password }) => {
-  return new Promise((resolve, reject) => {
-    datastore.find({ email, password }, (err, resultado) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(JSON.parse(resultado[0].administrador))
-    })
-  })
+exports.usuarioEhAdministrador = async ({ email, password }) => {
+  const resultado = await datastore.find({ email, password })
+  return JSON.parse(resultado[0].administrador)
 }
 
 exports.createUser = async body => {
-  return new Promise((resolve, reject) => {
-    datastore.insert(body, (err, novoUsuario) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(novoUsuario)
-    })
-  })
+  return datastore.insert(body)
 }
 
 exports.deleteById = async id => {
-  return new Promise((resolve, reject) => {
-    datastore.remove({ _id: id }, {}, (err, quantidadeRegistrosExcluidos) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(quantidadeRegistrosExcluidos)
-    })
-  })
+  return datastore.remove({ _id: id }, {})
 }
 
 exports.createOrUpdateById = async (idDoUsuarioQueSeraAlterado, body) => {
-  return new Promise((resolve, reject) => {
-    datastore.update({ _id: idDoUsuarioQueSeraAlterado }, body, { upsert: true }, (err, quantidadeRegistrosAlterados, registroCriado) => {
-      /* istanbul ignore if */
-      if (err) reject(err)
-      resolve(registroCriado)
-    })
-  })
+  return datastore.update({ _id: idDoUsuarioQueSeraAlterado }, body, { upsert: true, returnUpdatedDocs: true })
 }
