4e787f001e2627d259aad8cd2da8f64a65fec3ad
Paulo Gonçalves <paulorochag@hotmail.com>
2021-11-21 23:59:22 -0300

    test: dashboard

--- test/stryker.conf.js
+++ test/stryker.conf.js
@@ -3,16 +3,20 @@
  */
 module.exports = {
   mutate: [
-    'src/**/*.js',
-    '!src/server.js',
-    '!src/utils/logger.js',
-    '!src/utils/ambiente.js'
+    'src/middlewares/rate-limiter.js'
   ],
   thresholds: {
     high: 90,
     low: 60
   },
   reporters: ['html', 'dashboard', 'clear-text', 'progress'],
+  dashboard: {
+    project: 'github.com/ServeRest/ServeRest',
+    version: process.env.BRANCH,
+    baseUrl: 'dashboard.stryker-mutator.io/reports',
+    reportType: 'full',
+    module: undefined
+  },
   testRunner: 'command',
   commandRunner: {
     command: 'mocha --config test/integration/.mocharc.js'
