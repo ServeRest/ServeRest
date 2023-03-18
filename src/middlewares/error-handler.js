395f57041b55795839a538d4975f1ebd20510de8
Paulo Gonçalves <github@paulog.dev>
2023-03-18 02:42:22 +0000

    chore(patch): improve logging to catch error easier on google cloud

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -8,8 +8,8 @@ function errorHandler (error, _req, res, _next) {
   }
   /* istanbul ignore if */
   if (error.type === 'entity.parse.failed') {
-    console.log('Erro 500')
-    return res.status(500).json({
+    console.log('Entity parse error, user sending request without proper quotation marks.')
+    return res.status(400).json({
       message: 'Adicione aspas em todos os valores. Para mais informações acesse a issue https://github.com/ServeRest/ServeRest/issues/225'
     })
   }
@@ -17,6 +17,8 @@ function errorHandler (error, _req, res, _next) {
   if (error.code === 'ETIMEDOUT') {
     return res.status(408).json({ message: TIMEOUT })
   }
+  console.error('Error 500:', error)
+  console.error('Request:', _req)
   return res.status(500).json({ message: INTERNAL_ERROR, error })
 }
 
