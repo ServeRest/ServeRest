6dcec29fb8625efae8eac4aa692c70a62a6203b0
Paulo Gonçalves <github@paulog.dev>
2023-03-30 00:18:30 +0000

    fix: return correct status code to entity too large error

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -8,17 +8,21 @@ function errorHandler (error, _req, res, _next) {
   }
   /* istanbul ignore if */
   if (error.type === 'entity.parse.failed') {
-    console.log('Entity parse error, user sending request without proper quotation marks.')
+    console.log('lOG - Entity parse error, user sending request without proper quotation marks.')
     return res.status(400).json({
       message: 'Adicione aspas em todos os valores. Para mais informações acesse a issue https://github.com/ServeRest/ServeRest/issues/225'
     })
   }
   /* istanbul ignore if */
+  if (error.type === 'entity.too.large') {
+    return res.status(413).json({ message: 'Payload too large' })
+  }
+  /* istanbul ignore if */
   if (error.code === 'ETIMEDOUT') {
     return res.status(408).json({ message: TIMEOUT })
   }
-  console.error('Error 500:', error)
-  console.error('Request:', _req)
+  console.error('lOG - Error 500:', error)
+  console.error('lOG - Request:', _req)
   return res.status(500).json({ message: INTERNAL_ERROR, error })
 }
 
