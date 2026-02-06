22f609d4aa00f3cdb46edc494ee81d217ab4e72f
Paulo Gonçalves <29241659+PauloGoncalvesBH@users.noreply.github.com>
2026-02-06 18:32:08 -0300

    ci: forçar fim do teste que está travando antes do code coverage

--- test/integration/.mocharc.js
+++ test/integration/.mocharc.js
@@ -2,5 +2,6 @@ module.exports = {
   require: 'test/integration/utils/global.js',
   spec: ['test/integration/**/*.test.js'],
   color: true,
+  exit: process.env.CI === 'true',
   timeout: 30000
 }
